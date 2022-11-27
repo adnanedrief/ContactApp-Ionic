6d67a3f99d6977421564547bbb361df05b7fc21f
Adnane Drief <adnanedrief@gmail.com>
2022-11-27 22:46:02 +0100

    commit2

--- src/app/modal/Compte.ts
+++ src/app/modal/Compte.ts
@@ -1,7 +1,49 @@
 export class Compte {
-    email: string; // primary key
-    nom: string;
-    prenom: string;
-    password: string;
-    tel: string;
+     // email : primary key
+    private email: string;
+    private nom: string;
+    private prenom: string;
+    private password: string;
+    private tel: string;
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+
+    public getNom(): string {
+        return this.nom;
+    }
+
+    public setNom(nom: string): void {
+        this.nom = nom;
+    }
+
+    public getPrenom(): string {
+        return this.prenom;
+    }
+
+    public setPrenom(prenom: string): void {
+        this.prenom = prenom;
+    }
+
+    public getPassword(): string {
+        return this.password;
+    }
+
+    public setPassword(password: string): void {
+        this.password = password;
+    }
+
+    public getTel(): string {
+        return this.tel;
+    }
+
+    public setTel(tel: string): void {
+        this.tel = tel;
+    }
+
 }
--- src/app/modal/Contact.ts
+++ src/app/modal/Contact.ts
@@ -1,10 +1,76 @@
 export class Contacts {
-  nom: string;
-  prenom: string;
-  email: string;
-  tel: string;
-  ville: string;
-  adresse: string;
-  service: string;
-  compteEmail: number;//foreign  key
+  private  nom: string;
+  private prenom: string;
+  private email: string;
+  private tel: string;
+  private ville: string;
+  private adresse: string;
+  private service: string;
+  private compteEmail: number;
+  // compteEmail : //foreign  key
+
+    public getNom(): string {
+        return this.nom;
+    }
+
+    public setNom(nom: string): void {
+        this.nom = nom;
+    }
+
+    public getPrenom(): string {
+        return this.prenom;
+    }
+
+    public setPrenom(prenom: string): void {
+        this.prenom = prenom;
+    }
+
+    public getEmail(): string {
+        return this.email;
+    }
+
+    public setEmail(email: string): void {
+        this.email = email;
+    }
+
+    public getTel(): string {
+        return this.tel;
+    }
+
+    public setTel(tel: string): void {
+        this.tel = tel;
+    }
+
+    public getVille(): string {
+        return this.ville;
+    }
+
+    public setVille(ville: string): void {
+        this.ville = ville;
+    }
+
+    public getAdresse(): string {
+        return this.adresse;
+    }
+
+    public setAdresse(adresse: string): void {
+        this.adresse = adresse;
+    }
+
+    public getService(): string {
+        return this.service;
+    }
+
+    public setService(service: string): void {
+        this.service = service;
+    }
+
+    public getCompteEmail(): number {
+        return this.compteEmail;
+    }
+
+    public setCompteEmail(compteEmail: number): void {
+        this.compteEmail = compteEmail;
+    }
+
 }
