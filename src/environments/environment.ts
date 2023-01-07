f074e7251a8f1113f87ebd3df704629b93ff9a20
hamza <hamzaelhnait@gmail.com>
2023-01-07 14:54:43 +0100

    hamza commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyBrLQ2FXdKFCg2XEKbnWtahNHL9hIWnH3M',
-    authDomain: 'ionic-app-6c149.firebaseapp.com',
-    projectId: 'ionic-app-6c149',
-    storageBucket: 'ionic-app-6c149.appspot.com',
-    messagingSenderId: '1001389319408',
-    appId: '1:1001389319408:web:13f625dc60a8530c028e35',
-    measurementId: 'G-SQGXHTMLF6'
+    apiKey: 'AIzaSyDPMeDemNlH7hPR_h6MR81Uy-9IiEUgO1w',
+    authDomain: 'test-d6da3.firebaseapp.com',
+    projectId: 'test-d6da3',
+    storageBucket: 'test-d6da3.appspot.com',
+    messagingSenderId: '819328647357',
+    appId: '1:819328647357:web:958f4d3a97da21c0f3b7d4',
+    measurementId: 'G-CXBCK15T1R'
   }
 };
