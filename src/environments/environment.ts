c779078720c4811324d28f0a0a60548c5bfc1dc8
hamza <hamzaelhnait@gmail.com>
2023-01-08 01:57:30 +0100

    change for pull

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-<<<<<<< HEAD
+
     apiKey: 'AIzaSyDPMeDemNlH7hPR_h6MR81Uy-9IiEUgO1w',
     authDomain: 'test-d6da3.firebaseapp.com',
     projectId: 'test-d6da3',
@@ -9,14 +9,6 @@ export const environment = {
     messagingSenderId: '819328647357',
     appId: '1:819328647357:web:958f4d3a97da21c0f3b7d4',
     measurementId: 'G-CXBCK15T1R'
-=======
-    apiKey: 'AIzaSyBrLQ2FXdKFCg2XEKbnWtahNHL9hIWnH3M',
-    authDomain: 'ionic-app-6c149.firebaseapp.com',
-    projectId: 'ionic-app-6c149',
-    storageBucket: 'ionic-app-6c149.appspot.com',
-    messagingSenderId: '1001389319408',
-    appId: '1:1001389319408:web:4aaa5fa6976c8b52028e35',
-    measurementId: 'G-CK57983TRT'
->>>>>>> 7c64a1ef11731fc6bf05b8e28a488df4a3a0b269
+
   }
 };
